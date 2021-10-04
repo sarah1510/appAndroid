90926711549f62f7e06d7899fec4cf3e8ddaa593
SARA APARECIDA SILVA DE FARIA <21192881@CFP127ALUNO.LOCAL>
2021-10-04 11:24:18 -0300

    iniciando menu na tela de cadastro

--- app/src/main/java/com/example/primeiroapp/novo_usuario_activity.kt
+++ app/src/main/java/com/example/primeiroapp/novo_usuario_activity.kt
@@ -7,5 +7,10 @@ class novo_usuario_activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_novo_usuario)
+
+        supportActionBar!!.title = "Perfil"
+
+
+
     }
 }
