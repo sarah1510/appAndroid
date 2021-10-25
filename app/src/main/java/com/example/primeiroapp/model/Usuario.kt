9832131f7a16679ab4f8d69846c554cfa81dd26e
SARA APARECIDA SILVA DE FARIA <21192881@CFP127ALUNO.LOCAL>
2021-10-25 11:23:11 -0300

    Salvando um novo usuário

---
+++ app/src/main/java/com/example/primeiroapp/model/Usuario.kt
@@ -0,0 +1,15 @@
+package com.example.primeiroapp.model
+
+import java.time.LocalDate
+
+data class Usuario(
+    var id: Int,
+    var nome: String,
+    var email: String,
+    var senha: String,
+    var peso: Int,
+    var altura: Double,
+    var dataNacimento: LocalDate,
+    var profissao: String,
+    var sexo: Char
+)
--- app/src/main/java/com/example/primeiroapp/ui/NovoUsuarioActivity.kt
+++ app/src/main/java/com/example/primeiroapp/ui/NovoUsuarioActivity.kt
@@ -1,15 +1,22 @@
-package com.example.primeiroapp
+package com.example.primeiroapp.ui
 
 import android.app.DatePickerDialog
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.EditText
+import android.widget.RadioButton
 import android.widget.Toast
+import com.example.primeiroapp.R
+import com.example.primeiroapp.model.Usuario
+import com.example.primeiroapp.utils.convertStringToLocalDate
+import java.time.LocalDate
 import java.util.*
 
-class novo_usuario_activity : AppCompatActivity() {
+class NovoUsuarioActivity : AppCompatActivity() {
 
     lateinit var editEmail: EditText
     lateinit var editSenha: EditText
@@ -17,6 +24,8 @@ class novo_usuario_activity : AppCompatActivity() {
     lateinit var editProfissao: EditText
     lateinit var editAltura: EditText
     lateinit var editDataNascimento: EditText
+    lateinit var radioF: RadioButton
+    lateinit var radioM: RadioButton
 
 
 
@@ -30,6 +39,8 @@ class novo_usuario_activity : AppCompatActivity() {
         editProfissao = findViewById(R.id.edit_profissao)
         editAltura = findViewById(R.id.edit_altura)
         editDataNascimento = findViewById(R.id.et_data)
+        radioF = findViewById(R.id.radio_feminino)
+        radioM = findViewById(R.id.radio_masculino)
 
 
 
@@ -69,8 +80,62 @@ class novo_usuario_activity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (validarCampos()){
+            // Criar o objeto usuario
+                val nascimento = convertStringToLocalDate(editDataNascimento.text.toString())
 
+
+            val usuario = Usuario(
+                1,
+                editNome.text.toString(),
+                editEmail.text.toString(),
+                editSenha.text.toString(),
+                0,
+                editAltura.text.toString().toDouble(),
+                LocalDate.of(
+                    nascimento.year,
+                    nascimento.monthValue,
+                    nascimento.dayOfMonth
+                ),
+                editProfissao.text.toString(),
+                if(radioF.isChecked){
+                    'F'
+                } else {
+                    'M'
+                }
+
+                //Outra amneira de realizar (sem as chaves{}):
+                // if (radioF.isChecked) 'F' else 'M'
+            )
+
+            // Salvar o registro
+            // Em um SharedPreferences
+
+            // A istrução abaixo irá criar um
+            // arquivo shredPreferences se não existir
+            // Se existir ele será aberto para edição
+            val dados = getSharedPreferences(
+                "usuario", Context.MODE_PRIVATE)
+
+
+            // Vamos criar o objeto que permitirá a
+            // edição dos dados do arquivo SharedPreferences
+            val editor = dados.edit()
+
+            editor.putInt("id", usuario.id)
+            editor.putString("nome", usuario.nome)
+            editor.putString("email", usuario.email)
+            editor.putString("senha", usuario.senha)
+            editor.putInt("peso", usuario.peso)
+            editor.putFloat("altura", usuario.altura.toFloat())
+            editor.putString("dataNascimento", usuario.dataNacimento.toString())
+            editor.putString("profissao", usuario.profissao)
+            editor.putString("sexo", usuario.sexo.toString())
+            editor.apply()
         }
+
+        Toast.makeText(this, "Usuário cadastrado", Toast.LENGTH_SHORT).show()
+
+
         return true
     }
 
@@ -112,8 +177,6 @@ class novo_usuario_activity : AppCompatActivity() {
 
         return valido
         }
-
-    //VERIFICAR A VALIDAÇÃO
 
  }
 
